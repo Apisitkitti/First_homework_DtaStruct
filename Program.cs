7dd99884029b9ac914f1d63f961c76075aab889d
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2022-11-18 21:01:04 +0700

    success

--- Program.cs
+++ Program.cs
@@ -83,77 +83,93 @@ class Program
      static void Main()
     {
         int Num_G = 0;
-        int num_R =0;
-        int check =0;
+        char check_rose = 'b';
         Queue<char> queue1 = new Queue<char>();
         char flower = 'a';
         while(flower == 'J'|| flower =='G'|| flower == 'O'|| flower == 'R'|| flower == 'a')
         {
-            check ++;
+            
             flower = char.Parse(Console.ReadLine());
             
-          if(flower == 'J'|| flower =='G'|| flower == 'O'||flower == 'R')
-          {
-           
-            queue1.Push(flower);
+          if(flower == 'J' )
+          { 
+            
+            if(check_rose == flower)
+            {
+                
+                Console.WriteLine("Invalid pattern.");
+                check_rose = 'J';
+            }
+            else 
+            {
+                queue1.Push(flower);
+                check_rose ='b';
+            }
           }
             
-
+          if(flower == 'O' )
+          { 
             
+            if(check_rose == flower)
+            {
+                
+                Console.WriteLine("Invalid pattern.");
+                check_rose = 'O';
+            }
+            else 
+            {
+                queue1.Push(flower);
+                check_rose = 'b';
+            }
+          }
              
             else if(flower == 'G')
             {
+                if(check_rose == flower)
+                {
+                     Console.WriteLine("Invalid pattern.");  
+                     check_rose = 'G';
+                }
                 
-                if(Num_G <=2)
+                else if(Num_G <=2)
                 {
                     Num_G++;
                     queue1.Push(flower);
+                    check_rose = 'b';
                     
                 }
-                else if(Num_G >3)
-                {
-                    queue1.Pop();
-                  
-                }
                 else if(Num_G >2)
-                {
+                { 
                     Console.WriteLine("Invalid pattern.");
-                }            
-               
-                else
-                {
-                    queue1.Push(flower); 
-                }
+                      
+                }                
                 
    
             }
             
             else if(flower == 'R')
             {   
-              
+               
                 if(queue1.GetLength() == 0 )
                 {
                    Console.WriteLine("Invalid pattern."); 
-                }
-                else
+                }    
+                
+               
+                else if(queue1.GetLength() >0 )
                 {
-                    queue1.Push(flower);
-                }
-            }
-            
-          
+                      queue1.Push(flower);
+                      check_rose = queue1.Get(queue1.GetLength()-2);
            
-          
-          
+                }
+             
+            }
+     
         }
          for(int i =0; i< queue1.GetLength(); i++)
         {
             
             Console.Write("{0}",queue1.Get(i),queue1);
         }
-       
-       
-         
-        
     }
 }
